4f3dd10b534c0e473e143149a0581c2a1cd53e07
Krista Rutz <rutzkri000@gmail.com>
2020-03-13 09:40:18 -0700

    add boilerplate to class controllers

---
+++ VendorTracker/Controllers/VendorsController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorTracker.Models;
+
+namespace VendorTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+
+  }
+}
