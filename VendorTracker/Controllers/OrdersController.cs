adade6a793af026fa44248d39f5adf1dbe89ba4d
Krista Rutz <rutzkri000@gmail.com>
2020-03-13 10:37:38 -0700

    finish linking between order and vendor details

--- VendorTracker/Controllers/OrdersController.cs
+++ VendorTracker/Controllers/OrdersController.cs
@@ -7,8 +7,20 @@ namespace VendorTracker.Controllers
 {
   public class OrdersController : Controller
   {
+    [HttpGet("/vendors/0/items")]
+    public ActionResult Index()
+    {
+      return RedirectToAction("Show", "Vendors");
+    }
+
     [HttpGet("/vendors/0/items/new")]
     public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpGet("/vendors/0/items/0")]
+    public ActionResult Show()
     {
       return View();
     }
