4d8ccdaedeba03da47a6ce1d0a6a216cf8c45641
Krista Rutz <rutzkri000@gmail.com>
2020-03-13 11:32:37 -0700

    write logic for constructing new order rely mainly on auto-filled fields, including C# DateTime. User inputs bread and pastry number counts

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -1,12 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 namespace VendorTracker.Models
 {
   public class Order
   {
+    public string Description { get; set; }
+    public int Id { get; }
+    public int Bread { get; set; }
+    public int Pastries { get; set; }
+    public DateTime DateOrdered { get; }
+    public DateTime DateModified { get; set; }
+    public DateTime DateDue { get; set; }
+    public string Title { get; set; }
+    private static List<Order> _instances = new List<Order> { };
+
     public Order(int bread, int pastries)
     {
+      Bread = bread;
+      Pastries = pastries;
+      _instances.Add(this);
+      Id = _instances.Count;
+      DateOrdered = DateTime.Now;
+      DateModified = DateOrdered;
+      DateDue = DateModified.AddDays(2);
+      Title = $"Order No. {Id}, {DateModified.ToString()}";
+      Description = $"Bread: {Bread}, Pastries: {Pastries}";
+    }
 
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
     }
   }
 }
